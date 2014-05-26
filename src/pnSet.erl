6546f6ebff1177768ec6907aad9521006571153a
Sebastian Green-Husted <ricecake@tfm.nu>
2014-05-25 23:43:49 -0400

    flesh out the new method for pnsets

--- src/pnSet.erl
+++ src/pnSet.erl
@@ -1,13 +1,15 @@
 -module(pnSet).
 -export([new/0, lookup/2, add/2, remove/2, merge/2, apply/2]).
 
--record(orset, {}).
+-record(pnset, {store}).
 
 % operations should return {Status, NewStructure, Operation}
 % should make it pretty easy to share either the structure or
 % the operation, depending on needs
 
-new() -> ok.
+new() ->
+	Store = dict:new(),
+	#pnset{store = Store}.
 lookup(Self, Key) -> ok.
 add(Self, Key) -> ok.
 remove(Self, Key) -> ok.
