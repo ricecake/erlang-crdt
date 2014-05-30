d9a1abcaf8b8e15cdd771d5c5dcdc539e6c41800
Sebastian Green-Husted <ricecake@tfm.nu>
2014-05-29 23:50:21 -0400

    flesh out more of the pnset

--- src/pnSet.erl
+++ src/pnSet.erl
@@ -14,20 +14,23 @@ new() ->
 lookup(#pnset{store=Store} = Self, Key) ->
 	case dict:find(Key, Store) of
 		{ok, Value} when Value > 0 -> true;
-		{ok, Value} -> false;
+		{ok, Value} when Value < 1 -> false;
 		error -> false
 	end.
 
 add(#pnset{store = Store} = Self, Key) -> 
 	NewStore = dict:update_counter(Key, 1, Store),
-	Self#pnset{store = NewStore}.
+	{ok, Self#pnset{store = NewStore}, {add, Key}}.
 
 remove(#pnset{store = Store} = Self, Key) -> 
 	NewStore = dict:update_counter(Key, -1, Store),
-	Self#pnset{store = NewStore}.
+	{ok, Self#pnset{store = NewStore}, {del, Key}}.
 
 list(#pnset{store = Store} = State) -> 
 	[ Item || {Item, Count} <- dict:to_list(Store), Count > 0].
 
-merge(First, Second) -> ok.
+merge(#pnset{store = Fstore}, #pnset{store = Sstore}) ->
+	NewStore = lists:foldl(fun({Key, Count}, Store)-> dict:update_counter(Key, Count, Store) end, Fstore, dict:to_list(Sstore)),
+	#pnset{store = NewStore}.
+
 apply(State, Op) -> ok.
