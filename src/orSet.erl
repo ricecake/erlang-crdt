0e72ecf1fd4a84b60797f762be2310813b312ce0
Sebastian Green-Husted <ricecake@tfm.nu>
2014-06-01 11:36:37 -0400

    minor work on the orset

--- src/orSet.erl
+++ src/orSet.erl
@@ -1,15 +1,20 @@
 -module(orSet).
 -export([new/0, lookup/2, add/2, remove/2, merge/2, apply/2]).
 
--record(orset, {}).
+-record(orset, {store}).
 
 % operations should return {Status, NewStructure, Operation}
 % should make it pretty easy to share either the structure or
 % the operation, depending on needs
 
-new() -> ok.
+new() -> #orset{store = dict:new()}.
 lookup(Self, Key) -> ok.
-add(Self, Key) -> ok.
+add(#orset{store = Store}, Key) -> 
+	SubMap = case dict:find(Key, Store) of 
+		{ok, Value} -> Value;
+		error       -> dict:new()
+	end,
+	ok.
 remove(Self, Key) -> ok.
 merge(Self, Key) -> ok.
 apply(Self, Key) -> ok.
